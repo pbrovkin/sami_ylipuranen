7895a724ac7f3b785a5aced634624149b2687bb8
Bibek Dhakal <bbekdhakal@gmail.com>
2020-04-22 14:01:06 +0300

    plugins added and config resolved

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,15 +43,34 @@ module.exports = {
         },
       },
     },
-  ],
-  plugins: [
     {
-      resolve:'gatsby-source-contentful',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        spaceId:process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-
-      }
+        path: `${__dirname}/src`,
+        name: `src`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
     },
     {
       resolve: 'gatsby-plugin-mailchimp',
@@ -60,42 +79,41 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `gatsby-source-contentful`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        spaceId: `az44re8kag24`,
+        accessToken:`N8O-4Vq0O89bBs0sOTcq_3ZRLIoa4-RCuxapcEYxQSM`
       }
     },
-   
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve:'gatsby-transformer-remark',
-      options:{
-        plugins:[
-         
-            {
-              resolve: `gatsby-transformer-sharp`
-            },
-        
-          'gatsby-remark-relative-images',
-          {
-            resolve:'gatsby-remark-images',
-            options:{
-              maxwidth: 750,
-              linkImagesToOriginal: false
-
-            }
-          }
-        ]
-      }
-    }
-    
-     
-    // },'gatsby-transformer-remark'
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    `@contentful/gatsby-transformer-contentful-richtext`,
+  
+   
   ],
-  }
+  
+}
+
+
+
+
