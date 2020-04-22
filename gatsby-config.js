a733e57e9ee1657ae24bfb2ebd3f3afaab7ed980
Bibek Dhakal <bbekdhakal@gmail.com>
2020-04-22 13:30:28 +0300

    plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,66 +43,59 @@ module.exports = {
         },
       },
     },
+  ],
+  plugins: [
+    {
+      resolve:'gatsby-source-contentful',
+      options: {
+        spaceId:process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: "https://gmail.us4.list-manage.com/subscribe/post?u=5f53e9e0425e25be4ef97729a&amp;id=f7a1734428",
+      },
+    },
+    'gatsby-plugin-sass',
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src`,
         name: `src`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `az44re8kag24`,
-        accessToken:`N8O-4Vq0O89bBs0sOTcq_3ZRLIoa4-RCuxapcEYxQSM`
+        path: `${__dirname}/src/`,
       }
     },
-    `gatsby-transformer-sharp`,
+   
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    `gatsby-plugin-react-helmet`,
-    `@contentful/gatsby-transformer-contentful-richtext`,
-  
-   
+      resolve:'gatsby-transformer-remark',
+      options:{
+        plugins:[
+         
+            {
+              resolve: `gatsby-transformer-sharp`
+            },
+        
+          'gatsby-remark-relative-images',
+          {
+            resolve:'gatsby-remark-images',
+            options:{
+              maxwidth: 750,
+              linkImagesToOriginal: false
+
+            }
+          }
+        ]
+      }
+    }
+    
+     
+    // },'gatsby-transformer-remark'
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-  
-}
+  }
