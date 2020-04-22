561e302088e1c13f9e9744e85d2732dd695d4b5c
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2020-04-22 18:35:32 +0300

    delivery data to blogs from contentfull are ready

--- src/pages/blogs/blogs.js
+++ src/pages/blogs/blogs.js
@@ -1,4 +1,4 @@
-import React, { useState }  from "react"
+import React, {useState} from "react"
 import {Link, graphql} from "gatsby"
 import Layout from "../../components/layout"
 import styles from "./blog.module.css";
@@ -14,44 +14,42 @@ const Blog = ({data, location}) => {
   
   const [filter, setFilter] = useState('');
   
-  // const siteTitle = data.site.siteMetadata.title
-  // const posts = data.allContentfulPost.edges
+  const posts = data.allContentfulPost.edges
   
-  const post = [
-    {
-      id: 1,
-      title: '1Some Blog',
-      descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
-    },
-    {
-      id: 2,
-      title: '2Some Blog',
-      descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
-    },
-    {
-      id: 3,
-      title: '3Some Blog3',
-      descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
-    },
-    {
-      id: 4,
-      title: 'Some Blog4',
-      descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
-    },
-  ];
+  // const post = [
+  //   {
+  //     id: 1,
+  //     title: '1Some Blog',
+  //     descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: '2Some Blog',
+  //     descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: '3Some Blog3',
+  //     descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Some Blog4',
+  //     descText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eos ipsam, laborenisi placeat possimus repellendus'
+  //   },
+  // ];
   
-  const postsToShow = post.filter(post => post.title.toLowerCase().includes(filter.toLowerCase()))
+  const postsToShow = posts.filter(post => post.node.title.toLowerCase().includes(filter.toLowerCase()))
   
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   };
   
   
-  
   return (
       <>
-        <Filter onChange={handleFilterChange} value={filter}/>
-        <Layout>
+        <Filter onChange={handleFilterChange} value={filter} />
+        <Layout location={location}>
           <div className={styles.blogPage}>
             <div className={`container ${styles.blogContainer}`}>
               <div className={styles.titleBlock}>
@@ -68,32 +66,41 @@ const Blog = ({data, location}) => {
               </div>
               <div className={styles.blogBlock}>
                 <ul className={styles.list}>
-                  {postsToShow.map(i => <li key={i.id} className={styles.item}>
-                        <div className={styles.imgBlog}>
-                          <div className={styles.imgContainer}>
-                            <img className={styles.blogImg} src={imgBlog} alt="img" />
-                          </div>
-                        </div>
-                        <div className={styles.entriesBlog}>
-                          <div className={styles.timeBlog}>
-                            <svg className={styles.iconClock}>
-                              <use href={Sprite + '#clock'} />
-                            </svg>
-                            <div className={styles.date}>
-                              April 13, 2020
-                            </div>
-                          </div>
-                          <div className={styles.subtitleBlock}>
-                            <Subtitle subtitle={i.title} />
-                          </div>
-                          <div className={styles.entriesText}>
-                            <p>{i.descText}</p>
-                          </div>
-                          <div className={styles.entriesButton}>
-                            <Button label={'Reed more'} />
-                          </div>
-                        </div>
-                      </li>
+                  {postsToShow.map(({node}) => {
+                        const title = node.title || node.slug;
+                        
+                        return (
+                            <li key={node.slug} className={styles.item}>
+                              <div className={styles.imgBlog}>
+                                <div className={styles.imgContainer}>
+                                  <img className={styles.blogImg} src={imgBlog} alt="img" />
+                                </div>
+                              </div>
+                              <div className={styles.entriesBlog}>
+                                <div className={styles.timeBlog}>
+                                  <svg className={styles.iconClock}>
+                                    <use href={Sprite + '#clock'} />
+                                  </svg>
+                                  <div className={styles.date}>
+                                    April 13, 2020
+                                  </div>
+                                </div>
+                                <div className={styles.subtitleBlock}>
+                                  <Link to={node.slug}>
+                                    <Subtitle subtitle={title} />
+                                  </Link>
+                                </div>
+                                <div className={styles.entriesText}>
+                                  <p>{node.description}</p>
+                                </div>
+                                <div className={styles.entriesButton}>
+                                  <Button label={'Reed more'} />
+                                </div>
+                              </div>
+                            </li>
+                        
+                        )
+                      }
                   )}
                 </ul>
               </div>
