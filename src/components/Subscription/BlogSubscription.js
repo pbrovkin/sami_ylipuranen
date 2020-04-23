c2d00986e1b6a643a6107c43fd196f2d3aa7f82e
Bibek Dhakal <bbekdhakal@gmail.com>
2020-04-23 20:13:45 +0300

    newsletter greetings added

--- src/components/Subscription/BlogSubscription.js
+++ src/components/Subscription/BlogSubscription.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import addToMailchimp from 'gatsby-plugin-mailchimp';
-import  formStyles from '../Subscription/Subscription.module.scss'
+import  formStyles from './Subscription.module.css'
+import Greeting from './greeting'
 
 export default class Subscription extends React.Component {
     state = {
@@ -22,8 +23,8 @@ export default class Subscription extends React.Component {
     
         addToMailchimp(this.state.email,{NAME:this.state.name})
         .then(data=>{
-         this.state.isSubscribed=true
-          console.log(data);
+       this.setState({isSubscribed:true})
+        
          
         })
         .catch((error) => {
@@ -36,15 +37,19 @@ export default class Subscription extends React.Component {
         return (
             <div>
                 <div>
+                {
+                            this.state.isSubscribed ===true ? <Greeting/> :
                     <form className={formStyles.EmailListForm}  onSubmit={this._handleSubmit} >
+                   <label>Email</label> 
                     <input
                             type="email"
                             value={this.state.email}
                             onChange={(event) => this._handleChange(event)}
-                            placeholder="e-mail"
+                            placeholder="email@email.com"
                             name="email"
                             required pattern='[a-z0-9._%+-]+@[a-z0-9.-]+`?\.[a-z]{2,4}$'
                              />
+                             <label>Name</label>
                         <input
                             type="text"
                             value={this.state.name}
@@ -55,6 +60,7 @@ export default class Subscription extends React.Component {
                         
                         <button>Subscribe</button>
                     </form>
+    }
                 </div>
             </div>
         );
