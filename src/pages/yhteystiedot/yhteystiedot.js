b4870cc68c42ad473b026437b567e1227f2649e5
Zionknight-crypto <info.davidhauser@gmail.com>
2020-04-24 12:10:02 +0300

    random to random

--- src/pages/yhteystiedot/yhteystiedot.js
+++ src/pages/yhteystiedot/yhteystiedot.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 import styles from './contact.module.css';
-import Title from "../../components/Title";
+import Title from "../../components/title";
 import imgPerson from '../../img/S.png'
 
 const Yhteystiedot = () => {
