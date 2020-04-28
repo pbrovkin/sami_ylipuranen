767307c4f4acb93cb51e5520d1b4f4522cd01bce
pbrovkin <pbrovkin@gmail.com>
2020-04-28 17:36:47 +0300

    blog media added

--- src/templates/blog-post-contentful.js
+++ src/templates/blog-post-contentful.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {graphql} from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo/seo"
 import '../styles/global.css'
@@ -7,66 +7,66 @@ import styles from './blog-post-contentfull.module.css'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 
 
-// function returnImgTagOrVideo(url) {
-//   let fileExt = url.substring(url.lastIndexOf(".") + 1, url.length) || url
-//   if (fileExt === "jpg" || fileExt === "jpeg" || fileExt === "png" || fileExt === "gif") {
-//     return <img src={"https:" + url} alt="blog" />
-//   } else if (fileExt === "mp4") {
-//     return (
-//         <video width="100%" src={"https:" + url} loop autoPlay muted playsInline></video>
-//     )
-//   } else {
-//     return "invalid media"
-//   }
-// }
+const returnImgTagOrVideo = (url) => {
+  console.log(url)
+  let fileExt = url.substring(url.lastIndexOf(".") + 1, url.length) || url
+  if (fileExt === "jpg" || fileExt === "jpeg" || fileExt === "png" || fileExt === "gif") {
+    return <img src={"https:" + url} alt="blog" />
+  } else if (fileExt === "mp4") {
+    return (
+      <video width="100%" src={"https:" + url} loop autoPlay muted playsInline></video>
+    )
+  } else {
+    return "invalid media"
+  }
+}
 
-const BlogPostContentfulTemplate = ({data, pageContext, location}) => {
+const BlogPostContentfulTemplate = ({ data, pageContext, location }) => {
   const post = data.contentfulPost
   const siteTitle = data.site.siteMetadata.title
-  const {previousBlog, nextBlog} = pageContext
-  
+  const { previousBlog, nextBlog } = pageContext
+
   return (
-      <Layout location={location} title={siteTitle}>
-        <SEO
-            title={post.title}
-            description={post.description}
-        />
-        <div className={styles.blog}>
-          <div className={`container ${styles.blogContainer}`}>
-            <div className={styles.blogBtn}>
-              {previousBlog && (
-                  <AniLink fade duration={.3} className={styles.btnPrevious} to={previousBlog} rel="prev">
-                    ← Previous
-                  </AniLink>
-              )}
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.title}
+        description={post.description}
+      />
+      <div className={styles.blog}>
+        <div className={`container ${styles.blogContainer}`}>
+          <div className={styles.blogBtn}>
+            {previousBlog && (
+              <AniLink fade duration={.3} className={styles.btnPrevious} to={previousBlog} rel="prev">
+                ← Previous
+              </AniLink>
+            )}
+          </div>
+          <div className={styles.contentBlock}>
+            <div className={styles.date}>
+              <h3>
+                {post.date}
+              </h3>
             </div>
-            <div className={styles.contentBlock}>
-              <div className={styles.date}>
-                <h3>
-                  {post.date}
-                </h3>
-              </div>
-              <div className={styles.title}>
-                <h1>
-                  {post.title}
-                </h1>
-              </div>
-              <div className={styles.mediaBlock}>
-                {/*{returnImgTagOrVideo(post.media.file.url)}*/}
-                {/* <img src={"https:" + post.media.file.url} alt="blogimage" /> */}
-              </div>
-              <section dangerouslySetInnerHTML={{__html: post.content.childContentfulRichText.html}} />
+            <div className={styles.title}>
+              <h1>
+                {post.title}
+              </h1>
             </div>
-            <div className={styles.blogBtn}>
-              {nextBlog && (
-                  <AniLink fade duration={.3} className={styles.btnNext} to={nextBlog} rel="next">
-                    Next →
-                  </AniLink>
-              )}
+            <div className={styles.mediaBlock}>
+              <>{(post.media === null) ? null : returnImgTagOrVideo(post.media.file.url)}</>
             </div>
+            <section dangerouslySetInnerHTML={{ __html: post.content.childContentfulRichText.html }} />
+          </div>
+          <div className={styles.blogBtn}>
+            {nextBlog && (
+              <AniLink fade duration={.3} className={styles.btnNext} to={nextBlog} rel="next">
+                Next →
+              </AniLink>
+            )}
           </div>
         </div>
-      </Layout>
+      </div>
+    </Layout>
   )
 };
 
